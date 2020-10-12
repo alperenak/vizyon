a1615f171387e525800f550c02507374e68b69b1
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-10-12 09:47:30 +0300

    FIXED: syllabus data fixed

--- src/components/Dropdown/dropdown.js
+++ src/components/Dropdown/dropdown.js
@@ -7,41 +7,23 @@ export default function Dropdown({
   startingTime,
   endTime,
   avatar,
-  type = "syllabus",
 }) {
-  return <RenderDropdown type={type} />;
-}
-
-function RenderDropdown(props) {
-  const {
-    children,
-    teacher,
-    lessonName,
-    startingTime,
-    endTime,
-    avatar,
-    type = "syllabus",
-  } = props;
-  if (type === "syllabus") {
-    return (
-      <div className={styles.dropdown}>
-        {children}
-        <div className={styles.dropdownContent}>
-          <div className={styles.avatar}>
-            <img src={avatar} />
-          </div>
-          <div className={styles.lessonInformation}>
-            <div className={styles.teacher}>{teacher}</div>
-            <div className={styles.lessonName}>{lessonName}</div>
-          </div>
-          <div className={styles.lessonHours}>
-            <div className={styles.first}>{startingTime}</div>
-            <div className={styles.second}>{endTime}</div>
-          </div>
+  return (
+    <div className={styles.dropdown}>
+      {children}
+      <div className={styles.dropdownContent}>
+        <div className={styles.avatar}>
+          <img src={avatar} />
+        </div>
+        <div className={styles.lessonInformation}>
+          <div className={styles.teacher}>{teacher}</div>
+          <div className={styles.lessonName}>{lessonName}</div>
+        </div>
+        <div className={styles.lessonHours}>
+          <div className={styles.first}>{startingTime}</div>
+          <div className={styles.second}>{endTime}</div>
         </div>
       </div>
-    );
-  } else if (type === "select") {
-    return "";
-  }
+    </div>
+  );
 }
