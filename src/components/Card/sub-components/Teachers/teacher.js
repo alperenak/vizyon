a74ef649f1a2fb5e8e4a5068bec94bb781c3c468
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-11-11 02:44:28 +0300

    FIXED: classes avatar fixed

--- src/components/Card/sub-components/Teachers/teacher.js
+++ src/components/Card/sub-components/Teachers/teacher.js
@@ -65,22 +65,27 @@ export default function Teachers({ teachersData, classesData, userRole }) {
                 classesData.map((item) => {
                   return (
                     <div className={styles.teachersLabel}>
-                      <div className={styles.avatar}>
-                        <img
-                          src={
-                            item.instructor !== null && item.instructor
-                              ? item.instructor.profile_photo
-                                ? item.instructor.profile_photo.includes("http")
-                                  ? item.instructor.profile_photo
+                      <div className={styles.profileSection}>
+                        <div className={styles.avatar}>
+                          <img
+                            src={
+                              item.instructor !== null && item.instructor
+                                ? item.instructor.profile_photo
+                                  ? item.instructor.profile_photo.includes(
+                                      "http"
+                                    )
+                                    ? item.instructor.profile_photo
+                                    : teacherAvatar
                                   : teacherAvatar
                                 : teacherAvatar
-                              : teacherAvatar
-                          }
-                        />
+                            }
+                          />
+                        </div>
+                        <div className={styles.teacherInfo}>
+                          <div className={styles.name}>{item.name}</div>
+                        </div>
                       </div>
-                      <div className={styles.teacherInfo}>
-                        <div className={styles.name}>{item.name}</div>
-                      </div>
+
                       <div className={styles.icon}>
                         <More className={styles.moreIcon} />
                       </div>
